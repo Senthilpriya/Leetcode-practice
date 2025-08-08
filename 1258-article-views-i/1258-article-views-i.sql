6b3af4af2c0bdda003f37d4609fae5d9c5dcc1a2
Senthilpriya <138694981+Senthilpriya@users.noreply.github.com>
2025-08-08 22:25:56 +0530

    Time: 451 ms (27.47%), Space: 0B (100%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,5 +1,5 @@
-
-
-select distinct author_id as id from Views
-where author_id = viewer_id 
-order by id;
+# Write your MySQL query statement below
+SELECT distinct author_id as id
+FROM Views
+WHERE author_id = viewer_id 
+order by   id 
